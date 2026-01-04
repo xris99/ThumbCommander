35b9bc909a0609a0f0bb2ea0fc980cd0c3b06a4d
xris99 <79798089+xris99@users.noreply.github.com>
2026-01-04 14:49:04 +0100

    Delete pc_wrapper directory

--- pc_wrapper/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-PC Wrapper for ThumbCommander ThumbyColor Edition
-
-This package provides a wrapper to run the ThumbyColor version of ThumbCommander
-on a PC using Python and pygame. All game code remains unchanged.
-"""
-
-__version__ = "1.0.0"
